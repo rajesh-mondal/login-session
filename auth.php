8d30185d269d57fbead0776a76337fce2cbc1ca3
Rajesh Mondal <rajesh01749@gmail.com>
2023-10-18 16:57:08 +0600

    initial commit

---
+++ auth.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Hello World</title>
+        <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic">
+        <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.css">
+        <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/milligram/1.3.0/milligram.css">
+        <style>
+            body{
+                margin-top: 30px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="column column-60 column-offset-20">
+                    <h3>Simple Auth Example</h3>
+                </div>
+            </div>
+            <div class="row">
+                <div class="column column-60 column-offset-20">
+                </div>
+            </div>
+            <div class="row" style="margin-top:20px;">
+                <div class="column column-60 column-offset-20">
+                    <form method="POST">
+                        <label for="username">Username</label>
+                        <input type="text" name="username" id="username">
+                        <label for="password">Password</label>
+                        <input type="password" name="password" id="password">
+                        <button type="submit" class="button-primary" name="submit">Log In</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
